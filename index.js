3aa7324ff608312291d7cb4a4d2edb926deea444
raghavluthra10 <77231964+raghavluthra10@users.noreply.github.com>
2022-07-28 16:36:57 +0530

    setting state with ctx.state

--- index.js
+++ index.js
@@ -1,11 +1,15 @@
 const Koa = require("koa");
 const app = new Koa();
 
-app.use(async (ctx) => {
-  ctx.response.type = "text/plain";
-  ctx.body = "Hello";
+app.use(async (ctx, next) => {
+  ctx.state.foo = "foo";
+  console.log("ctx 1", ctx);
+  await next();
+});
 
-  console.log("ctx", ctx.response);
+app.use(async (ctx) => {
+  ctx.body = ctx.state.foo;
+  console.log("ctx 2", ctx);
 });
 
 app.listen(3000);
