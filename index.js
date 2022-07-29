e42774a0178786dfdc7cf7be00e2e131b97d6817
raghavluthra10 <77231964+raghavluthra10@users.noreply.github.com>
2022-07-29 23:05:25 +0530

    setting cookie

--- index.js
+++ index.js
@@ -2,14 +2,8 @@ const Koa = require("koa");
 const app = new Koa();
 
 app.use(async (ctx, next) => {
-  ctx.state.foo = "foo";
-  console.log("ctx 1", ctx);
-  await next();
-});
-
-app.use(async (ctx) => {
-  ctx.body = ctx.state.foo;
-  console.log("ctx 2", ctx);
+  ctx.body = ctx.cookies.get("foo");
+  console.log("ctx get req cookie", ctx);
 });
 
 app.listen(3000);
